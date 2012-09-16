b7a713b1f2e2c421ec3cf5447962deefdf91aeb4
shiv shankar dayal <shivshankar.dayal@gmail.com>
2012-09-16 18:05:36 +0530

    first commit

---
+++ CMakeFiles/auth1.wt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/auth1.wt.dir/Auth1.o"
+  "CMakeFiles/auth1.wt.dir/model/Session.o"
+  "CMakeFiles/auth1.wt.dir/model/User.o"
+  "auth1.wt.pdb"
+  "auth1.wt"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/auth1.wt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
